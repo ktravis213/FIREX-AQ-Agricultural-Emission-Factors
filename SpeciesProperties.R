16fb95a4b5ceb02cd6698ec19a1a36934eea8752
Katherine Travis <ktravis@fas.harvard.edu>
2022-09-29 11:17:35 -0400

    Initial commit: species properties

---
+++ SpeciesProperties.R
@@ -0,0 +1,154 @@
+# Species Properties
+# --------- Molecular weights -------
+mWCO2 = 44.01 ; mWCO = 28.01 ; mWCH4 = 16.04; mWNO = 30.01 ; mWNO2 = 46.0055 ; mWHONO = 47 
+mWOC = 12 ; mWCH2O = 30.031 ; mWC2H6 = 30.07 ; mWSO2 = 64.066 ; mWCHOCHO =58.04 ; mWCH3COCHO = 72.06
+mWN2O5 =108.01; mWBrCN = 105.921; mWBrCl =115.357
+mWBrO = 95.904; mWCH3COOCl = 78.5;  mWCl2 = 70.9 ;mWClNO2= 81.46
+mWHCN = 27.0253;  mWHCOOH= 46.03;  mWHNCO= 43.03 ;  mWHPMTF= 108.12
+mWPAN = 121.06 #C2H3NO5
+mWAPAN = 133.1#C3H3NO5
+mWPPN  = 135.08 #C3H5NO5
+mWPBN  = 183.12 #C7H5NO3
+mWMSA = 96.10 #CH4O3S
+mWClO4 = 100.46
+mWNH3 = MolecularWeight(formula=list(N=1, H=3))
+mWHNO3 = MolecularWeight(formula=list( O=3,N=1, H=1))
+mWHCN = MolecularWeight(formula=list( C=1,N=1, H=1))
+mWPROPENEHP = MolecularWeight(formula=list( C=3,O=3, H=8))
+mWPROPENEHN = MolecularWeight(formula=list( C=3,O=4, H=7, N=1))
+mWPAA = MolecularWeight(formula=list( C=2,O=3, H=4))
+mWIEPOX = MolecularWeight(formula=list( C=5,O=3, H=10))
+mWGLYC = MolecularWeight(formula=list( C=2,O=2, H=4))
+mWETHENEHN = MolecularWeight(formula=list(C=2, O=4, H=5, N=1))
+mWETHENEHP = MolecularWeight(formula=list(C=2, O=3, H=6))
+mWBUTENEHP = MolecularWeight(formula=list(C=4, O=3, H=10))
+mWBUTENEHN = MolecularWeight(formula=list(C=4, O=4, H=9, N=1))
+mWPHENOL = MolecularWeight(formula=list(C=6, O=1, H=6))
+mWH2O2 = MolecularWeight(formula=list( O=2, H=2))
+mWMHP = MolecularWeight(formula=list( C=1,O=2, H=4))
+mWISOPN = MolecularWeight(formula=list( C=5,O=4, H=9, N=1))
+mWISOPOOH = MolecularWeight(formula=list( C=5,O=3, H=10))
+mWC6H10O5 = MolecularWeight(formula=list(C=6,H=10, O=5))
+mWNH4 = MolecularWeight(formula=list(N=1,H=4))
+mWSO4 = MolecularWeight(formula=list(S=1,O=4))
+mWNO3 = MolecularWeight(formula=list(N=1,O=3))
+mWO3 = MolecularWeight(formula=list(O=3))
+mWI = MolecularWeight(formula=list(I=1))
+mWSSA = 31.4 #?
+mWCl = 35.453 ; mWBr = 79.904
+mWHNO2 = MolecularWeight(formula=list(H=1, N=1, O=2))
+mWCH3COCHO = MolecularWeight(formula=list(H=4, C=3, O=2))
+mWCHOCHO = MolecularWeight(formula=list(H=2, C=2, O=2))
+mWOCS=	60.075 #C1
+mWHFC152a=	148.91 #C1
+mWH1301=	148.910 #C1
+mWC2HCl3=	131.4 #C2
+mWCHBr2Cl=	208.28 #C1
+mWiPropONO2=	105.09 #C3
+mWx3Me2ButONO2=	MolecularWeight(formula=list(H=11, C=5, O=3)) #C5
+mWPropadiene=	40.0313 #C3
+mWt2Butene=	56.106 #C4
+mWx13Butadyine=	54.0916 #C 4
+mWIsoprene=	68.12 #C5
+mWX2Me1Butene=	70.13 #C5
+mWx1Hexene=	84.1608 #6
+mWnHeptane=	100.21 #7
+mWx23Dimebutane=	86.1754 #6
+mWx23DimePentane=	100.2 #7
+mWMeCycPentane=	84.16 #6
+mWToluene=	92.14 #7
+mWEthynylBenzene=	102.133 #8
+mWx2EthToluene=	120.19 #9
+mWbPinene=	136.23 #C10
+mWx2MeFuran=	82.1 #C5
+mWAcetonePropanal=	58.08 #C3
+mWNitromethane=61.04 #C1
+mWDMS= 62.14; mWCFC12= 120.91; mWCFC11= 137.37; mWCFC113= 187.375; mWCFC114=170.92
+mWHFC134a= 102.03;  mWHFC365mfc= 148;  mWHCFC22= 86.47;  mWHCFC142b= NaN;  mWHCFC141b= NaN; mWH2402= NaN;  mWH1211= 165.36
+mWCH3CCl3=MolecularWeight(formula=list(C=2,H=3,Cl=3))
+mWCCl4=MolecularWeight(formula=list(C=1,Cl=4))
+mWCHCl3=MolecularWeight(formula=list(C=1,H=1, Cl=3))
+mWCH2Cl2=MolecularWeight(formula=list(C=1,H=2, Cl=2))
+mWC2HCl3=MolecularWeight(formula=list(C=2,H=1, Cl=3))
+mWC2Cl4=MolecularWeight(formula=list(C=2, Cl=4))
+mWCH3Cl=MolecularWeight(formula=list(C=1, H=3, Cl=1))
+mWCH3Br=MolecularWeight(formula=list(C=1, H=3, Br=1))
+mWCH3I = MolecularWeight(formula=list(C=1, H=3, I=1)) 
+mWCH2Br2= MolecularWeight(formula=list(C=1, H=2, Br=2)) 
+mWCHBrCl2= MolecularWeight(formula=list(C=1, H=1,Cl=2, Br=2))
+mWCHBr2Cl= MolecularWeight(formula=list(C=1, H=1,Cl=1, Br=2))
+mWCHBr3 = MolecularWeight(formula=list(C=1, H=1, Br=3))
+mWCH2ClCH2Cl=MolecularWeight(formula=list(C=2, H=4, Cl=2))
+mWC2H5Cl=MolecularWeight(formula=list(C=2, H=5, Cl=1))
+mWMeONO2 = 77.04
+mWEthONO2 = 91.07
+mWnPropONO2 = 105.09; mWx2ButONO2 =119.12; mWx3PentONO2 = 133.15; mWx2PentONO2 = 133.15
+mWEthane = MolecularWeight(formula=list(C=2, H=6)); mWEthene = MolecularWeight(formula=list(C=2, H=4))
+mWEthyne = MolecularWeight(formula=list(C=2, H=2)); mWPropene =MolecularWeight(formula=list(C=3, H=6))
+mWPropane = MolecularWeight(formula=list(C=3, H=8))
+mWPropyne = MolecularWeight(formula=list(C=3, H=4)); 
+mWiButane = MolecularWeight(formula=list(C=4, H=10)); mWnButane = 58.1; mWx1Butene = 56.11; mWiButene = MolecularWeight(formula=list(C=4, H=8))
+mWc2Butene = 56.106; mWx13Butadiene = 54.0916; mWx12Butadiene = 54.09; mWx1Buten3yne = 52.07
+mWx1Butyne = 54.091; mWx2Butyne = 54.0904; mWiPentane = MolecularWeight(formula=list(C=5, H=12)); mWnPentane =MolecularWeight(formula=list(C=5, H=12))
+mWx1Pentene = 70.134; mWt2Pentene = 70.134; mWc2Pentene =70.134; mWX3Me1Butene = 70.13
+mWX2Me2Butene = 70.13; mWx13Pentadienes = 68.12; mWx3Me1PenteneAnd4Me1Pentene = 84.16
+mWx1Heptene = 98.1861; mWx1Octene = 112.24; mWx1Nonene = 126.24 ; mWx1Decene = 140.266; mWnHexane = 86.18 
+mWnOctane = 114.23; mWnNonane = 128.2; mWnDecane = 142.29; mWnUndecane = 156.31; mWx22Dimebutane = 86.17
+mWx2MePentane = 86.18; mWx3MePentane = 86.178; mWx2MeHexane = 100.2; mWx3MeHexane = 100.2
+mWx224TrimePentane =114.23; mWx234TrimePentane = 114.23; mWCycPentane =70.1 
+mWCycHexane =84.162; mWMeCycHexane = 98.186; mWCycPentene = 68.11; mWBenzene = MolecularWeight(formula=list(C=6, H=6))
+mWEthBenzene = MolecularWeight(formula=list(C=8, H=10)); mWmpXylene = 106.16 ; mWoXylene = 106.16; mWStyrene = 104.15
+mWiPropBenzene = 120.19; mWnPropBenzene =120.2 ; mWx3EthToluene = 120.19; mWx4EthToluene =120.19
+mWx135rimeBenzene = 120.19; mWx124rimeBenzene = 120.19; mWClBenzene =118.51; mWaPinene = 136.23
+mWTricyclene = MolecularWeight(formula=list(C=10, H=16)); mWCamphene = 136.24; mWMyrcene = 136.23; mWLimonene = 136.24; mWFuran =68.07
+mWx3MeFuran = 82.102; mWBenzFuran = 158.20; mWiButanal = 72.11; mWButanal = 72.11
+mWMEK = MolecularWeight(formula=list(C=4, H=8,O=1))
+mWMAC = 70.09; mWMVK = 70.09; mWAcrolein = MolecularWeight(formula=list(C=3, H=4, O=1)); mWiPropanol = 60.1
+mWAcrylonitrile = 53.06; mWPropNitrile = 55.08; mWMeAcetate = 74.08 
+mWdimebutane = 86.1754; mWdimepentane = 100.2
+mWch3cn = MolecularWeight(formula=list(C=2, H=3,N=1))
+mWmformate = 60.05 ; mW13Pentadiene=68.12
+mwPyrrole = 67.09 ; mWMBO = 86.13;
+mWCH3OH  = MolecularWeight(formula=list(C=1, H=4,O=1))
+mWC2H5OH= MolecularWeight(formula=list(C=2, H=6,O=1))
+mWAcetaldehyde = MolecularWeight(formula=list(C=2, H=4,O=1))
+mWpropanal = 58.08 ; mWfurfural = 96.0846
+mWMeFormate=60.05 ; mW2MeFuran=82.1 ;mW3MeFuran=82.1 
+mWEthynylBenzene = 102.136 ; mWCH3SH = 48.11 ; mWCS2 = 76.13
+mWCH2ClI = 176.38 ; mW2Butenals = 70.0898 ; mWMeAcrylonitrile = 100.121 
+mWSyringol = 154.16;mWGlycolaldehydeCH3COOH =60.052 ; mWHAC = 74.08
+mW2Furanone = 84.07336 ; mW23butanedione = 86.09; mWDimeFuran=96.13; mWMaleicAnhyd=98.06; mWBenzNitrile=103.04 
+mWBenzaldehyde = 106.12; mWC8Aromatics=mWoXylene; 
+mWC7H8O=108.14; mWCatecholx5MeFurfural=110.11
+mWC9Aromatics=120.2; mWC6H4O3=124.09; mWGuaiacol=124.14; mWNaphthalene=128.1705;mWMonoterpenes = mWLimonene
+mWCresol=108.14
+
+# -------- number of carbon atoms
+nCOCS=NaN;  nCDMS=NaN; nCCFC12=NaN; nCCFC11=NaN; nCCFC113=NaN; nCCFC114=NaN; nCHFC152a=NaN; nCHFC134a=NaN
+nCHFC365mfc=NaN; nCHCFC22=NaN; nCHCFC142b=NaN; nCHCFC141b=NaN;  nCH1301=NaN; nCH2402=NaN; nCH1211=NaN; nCCH3CCl3=NaN; nCCCl4=NaN; nCCHCl3=NaN; nCCH2Cl2=NaN; nCCH2ClI=NaN
+nCC2HCl3=NaN; nCC2Cl4=NaN; nCCH3Cl=NaN; nCCH3Br=NaN; nCCH3I=NaN; nCCH2Br2=NaN; nCCHBrCl2=NaN
+nCCHBr2Cl=NaN; nCCHBr3=NaN; nCCH2ClCH2Cl=NaN; nCC2H5Cl=NaN; nCMeONO2=1; nCEthONO2=2
+nCiPropONO2=3; nCnPropONO2=3; nCx2ButONO2=4; nCx3PentONO2=5; nCx2PentONO2=5
+nCx3Me2ButONO2=5; nCEthane=2; nCEthene=2; nCEthyne=2; nCPropene=3; nCPropane=3
+nCPropadiene=3; nCPropyne=3; nCiButane=4; nCnButane=4; nCx1Butene=4; nCiButene=4
+nCt2Butene=4; nCc2Butene=4; nCx13Butadiene=4; nCx12Butadiene=4; nCx1Buten3yne=5
+nCx13Butadyine=4; nCx1Butyne=4; nCx2Butyne=4; nCiPentane=5; nCnPentane=5
+nCIsoprene=5; nCx1Pentene=5; nCt2Pentene=5; nCc2Pentene=5; nCX3Me1Butene=4
+nCX2Me1Butene=5; nCX2Me2Butene=5; nCx13Pentadienes=5; nCx3Me1PenteneAnd4Me1Pentene=5
+nCx1Hexene=6; nCx1Heptene=7; nCx1Octene=8; nCx1Nonene=9; nCx1Decene=10; nCnHexane=6
+nCnHeptane=7; nCnOctane=8; nCnNonane=9; nCnDecane=10; nCnUndecane=11; nCx22Dimebutane=6
+nCx23Dimebutane=4; nCx2MePentane=5; nCx3MePentane=5; nCx2MeHexane=6; nCx3MeHexane=6
+nCx23DimePentane=5; nCx224TrimePentane=5; nCx234TrimePentane=5; nCCycPentane=5
+nCMeCycPentane=5; nCCycHexane=6; nCMeCycHexane=5; nCCycPentene=5; nCBenzene=6
+nCToluene=7; nCEthBenzene=8; nCmpXylene=8; nCoXylene=8; nCStyrene=8
+nCEthynylBenzene=8; nCiPropBenzene=9; nCnPropBenzene=9; nCx3EthToluene=9; nCx4EthToluene=9
+nCx2EthToluene=9; nCx135rimeBenzene=9; nCx124rimeBenzene=9; nCClBenzene=6; nCaPinene=10
+nCbPinene=10; nCTricyclene=10; nCCamphene=10; nCMyrcene=10; nCLimonene=10; nCFuran=4
+nCx2MeFuran=5; nCx3MeFuran=5; nCBenzFuran=8; nCiButanal=4; nCButanal=4
+nCAcetonePropanal=3; nCMEK=4; nCMAC=4; nCMVK=4; nCAcrolein=3; nCiPropanol=3
+nCNitromethane=1; nCAcrylonitrile=3; nCPropNitrile=3; nCMeAcetate=3
+nCMeFormate = 2
+nCx2ButONO2iButONO2=4; nCCH2ClI = 1; nCCS2 = 1; nCCH3SH = 1; nCCH2O = 1
+nCAcetaldehyde = 2; nCpropanal = 3; nCButanal=4; nC2Butenals=4
+nCCH3OH=1; nCC2H5OH=2 ; nCMBO =5; nCfurfural=5; nCHCN = 1; nCch3cn=2
+nCMeAcrylonitrile=4; nCPyrrole=4
