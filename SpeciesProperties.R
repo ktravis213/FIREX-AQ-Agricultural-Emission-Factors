1fb904f683eb06478a816d3687fa17447775d437
Katherine Travis <ktravis@fas.harvard.edu>
2023-02-10 15:29:02 -0500

    Fix a few typos

--- SpeciesProperties.R
+++ SpeciesProperties.R
@@ -3,8 +3,8 @@
 mWCO2 = 44.01 ; mWCO = 28.01 ; mWCH4 = 16.04; mWNO = 30.01 ; mWNO2 = 46.0055 ; mWHONO = 47 
 mWOC = 12 ; mWCH2O = 30.031 ; mWC2H6 = 30.07 ; mWSO2 = 64.066 ; mWCHOCHO =58.04 ; mWCH3COCHO = 72.06
 mWN2O5 =108.01; mWBrCN = 105.921; mWBrCl =115.357
-mWBrO = 95.904; mWCH3COOCl = 78.5;  mWCl2 = 70.9 ;mWClNO2= 81.46
-mWHCN = 27.0253;  mWHCOOH= 46.03;  mWHNCO= 43.03 ;  mWHPMTF= 108.12
+mWBrO = 95.904; mWCH3COOCl = MolecularWeight(formula=list(C=2,Cl=1, H=3,O=2));  mWCl2 = 70.9 ;mWClNO2= 81.46
+mWHCOOH= 46.03;  mWHNCO= 43.03 ;  mWHPMTF= 108.12
 mWPAN = 121.06 #C2H3NO5
 mWAPAN = 133.1#C3H3NO5
 mWPPN  = 135.08 #C3H5NO5
@@ -99,9 +99,9 @@ mWx224TrimePentane =114.23; mWx234TrimePentane = 114.23; mWCycPentane =70.1
 mWCycHexane =84.162; mWMeCycHexane = 98.186; mWCycPentene = 68.11; mWBenzene = MolecularWeight(formula=list(C=6, H=6))
 mWEthBenzene = MolecularWeight(formula=list(C=8, H=10)); mWmpXylene = 106.16 ; mWoXylene = 106.16; mWStyrene = 104.15
 mWiPropBenzene = 120.19; mWnPropBenzene =120.2 ; mWx3EthToluene = 120.19; mWx4EthToluene =120.19
-mWx135rimeBenzene = 120.19; mWx124rimeBenzene = 120.19; mWClBenzene =118.51; mWaPinene = 136.23
+mWx135rimeBenzene = 120.19; mWx124rimeBenzene = 120.19; mWClBenzene =MolecularWeight(formula=list(C=6, H=5,Cl=1)); mWaPinene = 136.23
 mWTricyclene = MolecularWeight(formula=list(C=10, H=16)); mWCamphene = 136.24; mWMyrcene = 136.23; mWLimonene = 136.24; mWFuran =68.07
-mWx3MeFuran = 82.102; mWBenzFuran = 158.20; mWiButanal = 72.11; mWButanal = 72.11
+mWx3MeFuran = 82.102; mWBenzFuran = 118.10; mWiButanal = 72.11; mWButanal = 72.11
 mWMEK = MolecularWeight(formula=list(C=4, H=8,O=1))
 mWMAC = 70.09; mWMVK = 70.09; mWAcrolein = MolecularWeight(formula=list(C=3, H=4, O=1)); mWiPropanol = 60.1
 mWAcrylonitrile = 53.06; mWPropNitrile = 55.08; mWMeAcetate = 74.08 
@@ -122,8 +122,7 @@ mWBenzaldehyde = 106.12; mWC8Aromatics=mWoXylene;
 mWC7H8O=108.14; mWCatecholx5MeFurfural=110.11
 mWC9Aromatics=120.2; mWC6H4O3=124.09; mWGuaiacol=124.14; mWNaphthalene=128.1705;mWMonoterpenes = mWLimonene
 mWCresol=108.14
-
-# -------- number of carbon atoms
+# -------- Number of carbon atoms --------
 nCOCS=NaN;  nCDMS=NaN; nCCFC12=NaN; nCCFC11=NaN; nCCFC113=NaN; nCCFC114=NaN; nCHFC152a=NaN; nCHFC134a=NaN
 nCHFC365mfc=NaN; nCHCFC22=NaN; nCHCFC142b=NaN; nCHCFC141b=NaN;  nCH1301=NaN; nCH2402=NaN; nCH1211=NaN; nCCH3CCl3=NaN; nCCCl4=NaN; nCCHCl3=NaN; nCCH2Cl2=NaN; nCCH2ClI=NaN
 nCC2HCl3=NaN; nCC2Cl4=NaN; nCCH3Cl=NaN; nCCH3Br=NaN; nCCH3I=NaN; nCCH2Br2=NaN; nCCHBrCl2=NaN
@@ -152,3 +151,184 @@ nCx2ButONO2iButONO2=4; nCCH2ClI = 1; nCCS2 = 1; nCCH3SH = 1; nCCH2O = 1
 nCAcetaldehyde = 2; nCpropanal = 3; nCButanal=4; nC2Butenals=4
 nCCH3OH=1; nCC2H5OH=2 ; nCMBO =5; nCfurfural=5; nCHCN = 1; nCch3cn=2
 nCMeAcrylonitrile=4; nCPyrrole=4
+
+
+# kOH
+# ----------- OH rate -----------
+TEMP=298 # K
+OHpropeneHP =  1.90E-12*exp(190/TEMP) # HYPROPO2H from the MCM
+OHetheneHP =  1.38E-11 #HYETHO2H from the MCM
+OHbuteneHP = 3E-11 #NBUTOLAOOH from the MCM
+OHch2o =  5.4E-12*exp(135/TEMP)
+OHc2h6 = 6.9E-12*exp(-1000/TEMP)
+OHch3chocho = 1.9E-12*exp(575/TEMP)
+OHchocho = 3.1E-12*exp(340/TEMP)
+OHhcooh = 4.5E-13 #MCM
+OHhnco=7E-16
+OHHCN=	2.20E-14
+OHCH2O=	9.37E-12
+OHCH3OH=	8.95E-13
+OHCH3CN=	2.00E-14
+OHCH3CHO=	1.50E-11
+OHC2H5OH=	3.20E-12
+OHAcrylonitrile=	4.00E-12
+OHAcrolein=	2.00E-11
+OHAcetonePropanal=	1.70E-13
+OHGlycolaldehydeCH3COOH=	3.68E-12
+OHCH3NO2=	2.00E-14
+OHDMS=	6.00E-12
+OHC4H5N=	1.11E-10
+OHFuran=	4.00E-11
+OHMVKMAC=	2.48E-11
+OHC4Carbonyls=	5.46E-12
+OHC3H6O2=	2.20E-12
+OHBenzene=	1.22E-12
+OHx2MeFuranx3MeFuran=	3.71E-11
+OHx2Furanone=	4.45E-11
+OHx23Butanedione=0.8E-12 # koss et al., 2018
+OHToluene=	5.63E-12
+OHPhenol=	2.80E-11
+OHFurfural=	3.56E-11
+OHDimeFurans=	1.32E-10
+OHMaleicAnhyd=	3.93E-13
+OHBenzNitrile=	1.00E-12
+OHStyrene=	5.80E-11
+OHBenzaldehyde=	1.20E-11
+OHC8Aromatics=	1.32E-11
+OHC7H8O=	2.62E-11
+OHCatecholx5MeFurfural=	8.01E-11
+OHBenzFuran=	3.70E-11
+OHC9Aromatics=	2.20E-11
+OHC6H4O3=	4.60E-12
+OHGuaiacol=	7.50E-11
+OHNaphthalene=	2.30E-11
+OHMonoterpenes=	1.63E-10
+OHCreosols=	1.00E-10
+OHSyringol=1.00E-10
+OHHPMTF=1.4E-12# VERES et al, https://www.pnas.org/content/117/9/4505
+#KMT15 from MCM
+M = (6.022E23)*(1.013E5)/(8.31*TEMP)/1E6
+K150 = 8.6E-29*M*(TEMP/300)^-3.1
+K15I=9.0E-12*(TEMP/300)^-0.85
+KR15 = K150/K15I
+FC15 = 0.48
+NC15 = 0.75-1.27*(log10(FC15))
+F15=10^(log10(FC15)/(1+(log10(KR15)/NC15)^2))
+OHEthene = (K150*K15I)*F15/(K150+K15I)
+# KMT17 from MCM
+K170=	5.0E-30*M*(TEMP/300)^-1.5	
+K17I=	1.0E-12
+KR17=	K170/K17I	
+FC17=	0.17*exp(-51/TEMP)+exp(-TEMP/204)	
+NC17=	0.75-1.27*(log10(FC17))	
+F17	=10^(log10(FC17)/(1.0+(log10(KR17)/NC17)^2))	
+OHEthyne = 	(K170*K17I*F17)/(K170+K17I)
+#KMT 16
+K160=	8E-27*M*(TEMP/300)^-3.5	
+K16I=	3.0E-11*(TEMP/300)^-1	
+KR16=	K160/K16I	
+FC16=	0.5	
+NC16=	0.75-1.27*(log10(FC16))	
+F16	=10^(log10(FC16)/(1+(log10(KR16)/NC16)^2))	
+OHPropene	=(K160*K16I)*F16/(K160+K16I)
+OHPropane=7.6E-12*exp(-585/TEMP)
+OHiButane=3.70E-12
+OHnButane = 2.44E-17*TEMP^2*exp(183/TEMP)
+OHx1Butene=6.6E-12*exp(465/TEMP)
+OHx13Butadiene=1.48E-11*exp(448/TEMP)
+OHnPentane=2.44E-17*TEMP^2*exp(183/TEMP)
+OHIsoprene=2.70E-11*exp(390/TEMP)
+OHx1Pentene=5.86E-12*exp(500/TEMP)
+OHx1Hexene=3.70E-11
+OHnHexane=1.53E-17*TEMP^2*exp(414/TEMP)
+OHnHeptane = 1.59E-17*TEMP^2*exp(478/TEMP)
+OHnOctane = 2.76E-17*TEMP^2*exp(378/TEMP)
+OHnNonane = 2.51E-17*TEMP^2*exp(447/TEMP)
+OHnDecane=3.13E-17*TEMP^2*exp(416/TEMP)
+OHnUndecane=1.29E-11
+OHx22Dimebutane=3.22E-11*exp(-781/TEMP)
+OHx23Dimebutane=1.24E-17*TEMP^2*exp(494/TEMP) 
+OHx2MePentane=5.30E-12
+OHx3MePentane=5.40E-12
+OHx2MeHexane=6.86E-12 
+OHx3MeHexane=7.15E-12
+OHEthBenzene = 7.00E-12
+OHmpXylene = 2.31E-11 # OH for m xylene 
+OHoXylene = 1.36E-11
+OHStyrene =5.80E-11
+OHaPinene = 1.2E-11*exp(440/TEMP)
+OHbPinene=2.38E-11*exp(357/TEMP)
+OHLimonene = 4.28E-11*exp(401/TEMP)
+OHButanal=6.0E-12*exp(410/TEMP)
+OHMEK = 1.5E-12*exp(-90/TEMP)
+OHMAC = 8.0E-12*exp(380/TEMP)
+OHMVK = 2.6E-12*exp(610/TEMP)
+OHMeAcetate=8.54E-19*TEMP^2*exp(455/TEMP)
+OHEthONO2=6.7E-13*exp(-395/TEMP)
+OHiPropBenzene=5.80E-12 # MCM Propylbenzene
+OHnPropBenzene=5.80E-12 #MCM Propylbenzene
+OHx2EthToluene=1.19E-11
+# unknown monoterpenes -
+OHTricyclene= 2.86E-12 #https://onlinelibrary.wiley.com/doi/epdf/10.1002/kin.550241108
+OHCamphene=5.33E-11  #https://www.sciencedirect.com/science/article/pii/096016869090144C
+OHMyrcene=2.3E-10 #https://acp.copernicus.org/preprints/acp-2021-556/
+#Other unknown
+OHx2MeFuran=7.31E-11 #https://pubmed.ncbi.nlm.nih.gov/21309547/
+OHx3MeFuran=8.73E-11 #https://pubmed.ncbi.nlm.nih.gov/21309547/
+OHPropyne=5.71E-12 #https://www.researchgate.net/publication/231405918_Rate_constants_and_mechanisms_for_the_reaction_of_OH_OD_radicals_with_acetylene_propyne_and_2-butyne_in_air_at_297_2_K
+OHx2Butyne=3.01E-11 #https://www.researchgate.net/publication/231405918_Rate_constants_and_mechanisms_for_the_reaction_of_OH_OD_radicals_with_acetylene_propyne_and_2-butyne_in_air_at_297_2_K
+OHiPropanol= 3.21E-12#https://pubs.acs.org/doi/pdf/10.1021/j100501a001
+OHiButanal=6.0E-12*exp(410/TEMP) # MCM, C3H7CHO
+OHC2H5Cl=7.78E-18*TEMP^2*exp(-152/TEMP)
+OHiPentane=3.70E-12
+OHMBO=8.1E-12*exp(610/TEMP) # MCM
+OHpyrrole=1.20E-10 #https://www.sciencedirect.com/science/article/pii/0004698184901963
+OHpropanal=4.9E-12*exp(405/TEMP)
+OHMeONO2=4.0E-13*exp(-845/TEMP)
+
+OHOCS=NaN;  #Carbonyl sulfide 
+OHCFC12=NaN; OHCFC11=NaN; OHCFC113=NaN; OHCFC114=NaN; 
+OHHFC152a=NaN; OHHFC134a=NaN; OHHFC365mfc=NaN; OHHCFC22=NaN; OHHCFC142b=NaN; OHHCFC141b=NaN;
+OHH1301=NaN; OHH2402=NaN; OHH1211=NaN; OHCH3CCl3=NaN; OHCCl4=NaN; OHCHCl3=NaN; OHCH2Cl2=NaN;
+OHC2HCl3=NaN; OHC2Cl4=NaN; OHCH3Cl=NaN; OHCH3Br=NaN; OHCH3I=NaN; OHCH2Br2=NaN; OHCHBrCl2=NaN;
+OHCHBr2Cl=NaN; OHCHBr3=NaN; OHCH2ClCH2Cl=NaN; 
+
+OHiPropONO2=NaN; OHnPropONO2=NaN; OHx2ButONO2=NaN; OHx3PentONO2=NaN; OHx2PentONO2=NaN;
+OHx3Me2ButONO2=NaN; OHPropadiene=8.51E-22*TEMP^3.05 *exp(2215/TEMP); #https://pubs.acs.org/doi/10.1021/acs.jpca.6b04387
+OHiButene=9.4E-12*exp(505/TEMP); # MCM for isobutene
+OHt2Butene=1.01E-11*exp(550/TEMP); # MCM
+OHc2Butene=1.10E-11*exp(487/TEMP); # MCM 
+OHc2Pentene=6.54E-11;OHt2Pentene=6.69E-11;OHx135rimeBenzene=5.67E-11;OHX2Me1Butene=6.07E-11;OHX3Me1Butene=5.32E-12*exp(533/TEMP)# MCM
+
+#www.atmos-chem-phys.net/15/13915/2015/
+OHx1Octene=36E-12; OHx1Heptene=38E-12;OHx1Decene=46E-12;  OHMeCycHexane=9.6E-12;OHx12Butadiene=27E-12; OHx1Buten3yne=20E-12;
+OHx13Butadyine=16E-12; OHx1Butyne=8E-12; OHX2Me2Butene=87E-12; OHx13Pentadienes=83E-12; OHx3Me1PenteneAnd4Me1Pentene=55E-12; # closest is Pentene_1_2methyl
+OHx1Nonene=42E-12
+OHx23DimePentane=5.2E-12; OHx224TrimePentane=5.2E-12; #Pentane_3methyl
+OHx234TrimePentane=OHx224TrimePentane; OHCycPentane=4.8E-12;
+OHMeAcrylonitrile=4E-12 # closest Propenenitrile_2 (Acrylonitrile)
+OHCycHexane=7E-12; OHCycPentene=57E-12; OHNitromethane=0.02E-12 
+OHEthynylBenzene=1E-12;OHx124rimeBenzene=32E-12; OHMeCycPentane=8.6E-12; OHt13Pentadiene=83E-12;OHx2Butenals=35E-12
+
+OHx3EthToluene=1.92e-11 #https://pubchem.ncbi.nlm.nih.gov/compound/3-Ethyltoluene#section=Vapor-Pressure
+OHx4EthToluene=1.21e-11 #https://pubchem.ncbi.nlm.nih.gov/compound/4-Ethyltoluene#section=Vapor-Pressure
+OHClBenzene=1.01E-11*exp(-2490/TEMP) #https://pubs.acs.org/doi/abs/10.1021/jp9049186
+OHPropNitrile=1.94e-13 #https://pubchem.ncbi.nlm.nih.gov/compound/Propionitrile#section=Henrys-Law-Constant
+OHCS2=1.5E-15 #https://pubs.acs.org/doi/10.1021/j100457a003 carbon disulfide
+OHCH3SH=3.29e-11 #https://pubchem.ncbi.nlm.nih.gov/compound/methanethiol
+OHCH2ClI=NaN
+OHMeFormate=9.39E-13*exp(-461/TEMP) #MCM
+OHx24DiMePentane=5.16e-12 # https://pubchem.ncbi.nlm.nih.gov/compound/2_4-Dimethylpentane#section=Vapor-Pressure
+
+OHH2O2 = 2.9E-12*exp(-160/298) # MCM
+
+K120	=2.5E-31*M*(TEMP/300)^(-2.6)	
+K12I=	2.0E-12	
+KR12=	K120/K12I
+FC12=	0.53	
+NC12=	0.75-1.27*(log10(FC12))	
+F12	=10^(log10(FC12)/(1.0+(log10(KR12)/NC12)^2))
+OHSO2 =(K120*K12I*F12)/(K120+K12I) #MCM
+
+OHOCS = 5.3E-15 #https://www.sciencedirect.com/science/article/pii/S0009261417302178
+
